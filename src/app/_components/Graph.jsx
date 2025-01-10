65c2aa4625a7701d6f617d65607ee0a4b549fe22
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-01-11 03:23:44 +0530

    feat: Add TradingView advanced chart widget component with customizable range and symbol

---
+++ src/app/_components/Graph.jsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useEffect, useRef, memo } from "react";
+
+function Graph({ range = "5D", symbol = "MARKETSCOM:BITCOIN" }) {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "autosize": true,
+        "symbol": "${symbol}",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "3",
+        "locale": "en",
+        "hide_top_toolbar": true,
+        "hide_legend": true,
+        "range": "${range}",
+        "allow_symbol_change": false,
+        "save_image": false,
+        "calendar": false,
+        "hide_volume": true,
+        "support_host": "https://www.tradingview.com"
+      }`;
+    container.current.appendChild(script);
+
+    return () => {
+      container.current.innerHTML = "";
+    };
+  }, [range, symbol]);
+
+  return (
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
+    </div>
+  );
+}
+
+export default Graph;
