5582e6969ab441f4021cec662bd422c59b5d1e67
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-01-12 00:49:45 +0530

    feat: add dynamic Page component for individual cryptocurrency details

---
+++ src/app/cryptocurrencies/[id]/page.tsx
@@ -0,0 +1,52 @@
+import { GetStartedCard } from "../.././_components/GetStartedCard";
+import { Navbar } from "../../_components/Navbar";
+import React from "react";
+import { TrendingCoins } from "../.././_components/TrendingCoins";
+import { Breadcrumb } from "../.././_components/Breadcrumbs";
+import SentimentDashboard from "../.././_components/Sentiment";
+import About from "../.././_components/About";
+import Tokenomics from "../.././_components/Tokenomics";
+import Team from "../.././_components/Team";
+import Footer from "../.././_components/Footer";
+import Overview from "../.././_components/Overview";
+import { CryptoDataFetcher } from "../.././_components/CryptoDataFetcher";
+
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ id: string }>
+  }) {
+    const slug = (await params).id
+    console.log(slug)
+    
+  return (
+    <div>
+      <Navbar />
+      <div className="mx-10 mt-2">
+        <Breadcrumb />
+      </div>
+      <div className="flex flex-wrap items-start justify-center lg:justify-between mx-0">
+        <div className="w-full lg:w-[70%] mb-4 lg:mb-0">
+          <CryptoDataFetcher
+            cryptoId={slug}
+            cryptoSymbol="BTC"
+            cryptoName="Bitcoin"
+          />{" "}
+          <Overview />
+          <SentimentDashboard />
+          <About />
+          <Tokenomics />
+          <Team />
+        </div>
+
+        <div className="w-full lg:w-[29%] flex flex-col gap-4 lg:ml-3 mx-5 sm:mx-0">
+          <div className="mb-2 lg:mb-2 mr-0 lg:mr-10">
+            <GetStartedCard />
+          </div>
+          <TrendingCoins />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
