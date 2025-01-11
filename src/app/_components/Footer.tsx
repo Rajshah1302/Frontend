ae9d6fda798abd894542438d25fc18d8ca146397
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-01-11 15:22:22 +0530

    feat: initialize footer and update page.tsx layout

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+      <footer className="bg-primary text-white text-center py-4">
+        <p>
+          © 2021 <strong>Crypto</strong>. All rights reserved.
+        </p>
+      </footer>
+    );
+}
